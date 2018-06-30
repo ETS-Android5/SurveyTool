98d0f1f54bda5d7c290187ad609b7d7f2026e818
jhes <ooxfordck@gmail.com>
2018-06-30 18:16:39 +0800

    add original package support with API 26
    update android build tool to API 2.3.3

--- BST_Tool_Canvas/app/src/main/java/com/hkm/bst_tool_canvas/MainActivity.java
+++ BST_Tool_Canvas/app/src/main/java/com/hkm/bst_tool_canvas/MainActivity.java
@@ -4,9 +4,13 @@ import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.hkm.bst_tool_canvas.NodeManagement.NodeMgm;
 
 import java.util.Timer;
 
@@ -16,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private Button preference_btn, record_plan, jobtask_btn, myaccoun;
     private int million_sec;
     private Timer timerThread;
+    private AppCompatButton mAppCompatButton;
     private FragmentActivity mRootContext;
     private final String TAG = "fonef";
 
@@ -28,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         online_status = (TextView) findViewById(R.id.statustext);
+        mAppCompatButton = (AppCompatButton) findViewById(R.id.big_small);
         final Bundle b = getIntent().getExtras();
         try {
             float Lat = Float.parseFloat(b.getString("latitude", ""));
@@ -36,6 +42,12 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             Log.d(TAG, e.toString());
         }
+        mAppCompatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NodeMgm.startInit(MainActivity.this);
+            }
+        });
         setUpMapIfNeeded();
     }
 
--- BST_Tool_Canvas/app/src/main/java/com/hkm/bst_tool_canvas/NodeManagement/NodeMgm.java
+++ BST_Tool_Canvas/app/src/main/java/com/hkm/bst_tool_canvas/NodeManagement/NodeMgm.java
@@ -1,6 +1,8 @@
 package com.hkm.bst_tool_canvas.NodeManagement;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 
 import com.hkm.bst_tool_canvas.R;
@@ -9,13 +11,22 @@ import com.hkm.surveytool.AppDataRetain;
 
 import butterknife.BindView;
 
-public class NodeMgm extends Mainase {
+public final class NodeMgm extends Mainase {
     @BindView(R.id.mlist_container)
     protected RecyclerView listview;
+    public static final int requestCode = 9312;
 
     @Override
     protected int layout() {
         return R.layout.c_list_node_classic;
+    }
+
+    public static final void startInit(AppCompatActivity c) {
+        Intent in = new Intent(c, NodeMgm.class);
+        Bundle work = new Bundle();
+        work.putString("msg", "sasaas");
+        in.putExtras(work);
+        c.startActivityForResult(in, requestCode);
     }
 
     @Override
